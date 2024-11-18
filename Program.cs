58406e7fced0e85c52cdd83de7d686b8ccdf20c0
ElisaRumSolberg <rumeysaince07@gmail.com>
2024-11-18 12:50:07 +0100

    Step 1: Hello World console app

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, C# world!");
+        Console.WriteLine("This is my first step in C#.");
+    }
+}
